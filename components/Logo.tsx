9cbe7da1c11a4e43c565b2907b43ec411ffbc631
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2025-02-06 23:05:19 +0530

    added nav

---
+++ components/Logo.tsx
@@ -0,0 +1,33 @@
+import { cn } from '@/lib/utils'
+import { SquareMousePointer } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+const Logo = ({fontSize = "2xl", iconSize = 20}:{
+    fontSize ?: string,
+    iconSize ?: number
+}) => {
+  return (
+    <Link href="/" className={cn("text-2xl font-extrabold flex items-center gap-2 ",
+        fontSize
+    )}>
+        <div className="rounded-xl bg-gradient-to-r from-emerald-500 to-emerald-600
+        p-2">
+            <SquareMousePointer size={iconSize} className='stroke-white'/>
+
+         
+
+        </div>
+        <div className="">
+                <span className='bg-gradient-to-r from-emerald-500 to-emerald-600 bg-clip-text text-transparent'>
+                    Flow
+                </span>
+                <span className='text-stone-700 dark:text-stone-300'>
+                    Scrape
+                </span>
+            </div>
+    </Link>
+  )
+}
+
+export default Logo
---
+++ components/SideBar.tsx
@@ -0,0 +1,113 @@
+"use client"
+import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from 'lucide-react'
+import React, { useState } from 'react'
+import Logo from './Logo'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+import { usePathname } from 'next/navigation'
+
+const routes = [
+  {
+    href: "/",
+    label: "Home",
+    icon: HomeIcon
+  },
+  {
+    href: "/workflows",
+    label: "Workflows",
+    icon: Layers2Icon
+  },
+  {
+    href: "/credentials",
+    label: "Credentials",
+    icon: ShieldCheckIcon
+  },
+  {
+    href: "/billing",
+    label: "Billing",
+    icon: CoinsIcon
+  }
+]
+
+const DesktopSidebar = () => {
+  const pathname = usePathname()
+
+  // Find the route that exactly matches the pathname or falls back to the home route.
+  const activeRoute = routes.find(route => route.href !== "/" && pathname.includes(route.href)) || routes[0]
+
+  return (
+    <div className="hidden relative md:block max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2">
+      <div className="flex items-center justify-center gap-2 border-b border-separate p-4">
+        <Logo />
+      </div>
+
+      <div className="p-2">TOTO CREDITS</div>
+
+      <div className="flex flex-col p-2">
+        {routes.map(route => (
+          <Link
+            key={route.href}
+            href={route.href}
+            className={buttonVariants({
+              variant: activeRoute.href === route.href ? "sidebarActiveItem" : "sidebarItem"
+            })}
+          >
+            <route.icon size={20} />
+            {route.label}
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default DesktopSidebar
+
+
+
+
+import {  MenuIcon,  } from 'lucide-react'
+
+
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+
+
+
+export function MobileSidebar() {
+  const pathname = usePathname()
+  const [isOpen, setOpen] = useState(false)
+
+  const activeRoute = routes.find(route => route.href !== "/" && pathname.includes(route.href)) || routes[0]
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <button aria-label="Open navigation menu">
+              <MenuIcon />
+            </button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px] space-y-4" side="left">
+            <Logo />
+            <div className="flex flex-col gap-1">
+              {routes.map(route => (
+                <Link
+                  key={route.href}
+                  href={route.href}
+                  className={buttonVariants({
+                    variant: activeRoute.href === route.href ? "sidebarActiveItem" : "sidebarItem"
+                  })}
+                  onClick={() => setOpen(false)}
+                >
+                  <route.icon size={20} />
+                  {route.label}
+                </Link>
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+      </nav>
+    </div>
+  )
+}
